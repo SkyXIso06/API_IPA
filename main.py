379ea0db60b036636406abc706e405edb5ce65ee
Eduardo Vanoye Chi <eduardovanoye2330@gmail.com>
2026-06-08 13:58:24 -0600

    Remove hardcoded JWT_SECRET_KEY
    Removed hardcoded JWT secret key for security.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import mysql.connector as mc
 from flask_jwt_extended import JWTManager
 from controladores.auth_controlador import auth_bp
 app = Flask(__name__)
-app.config["JWT_SECRET_KEY"]="123456789"
+app.config["JWT_SECRET_KEY"]=""
 jwt=JWTManager(app)
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(usuario_bp, url_prefix="/usuario")
